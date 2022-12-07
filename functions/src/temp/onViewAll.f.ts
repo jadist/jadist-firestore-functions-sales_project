fbf97becf9d664b34625fc72a562992ca1df794a
Eldest Pasirula <aknutman@gmail.com>
2022-12-07 22:41:48 +0700

    cleansing

--- functions/src/temp/onViewAll.f.ts
+++
@@ -1,6 +0,0 @@
-import * as functions from "firebase-functions";
-
-module.exports = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs! - by EAP", { structuredData: true });
-  response.send("Uhuuuuuyyyy........");
-});
