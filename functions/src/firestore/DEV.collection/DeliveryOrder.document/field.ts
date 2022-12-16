82768aee3cac3ad9d450e160c27891ebf9d5d880
Eldest Pasirula <aknutman@gmail.com>
2022-12-17 00:39:33 +0700

    Updated DeliveryOrder.document.field

--- functions/src/firestore/DEV.collection/DeliveryOrder.document/field.ts
+++ functions/src/firestore/DEV.collection/DeliveryOrder.document/field.ts
@@ -1,7 +1,11 @@
-export interface field {
-    TransactionNoInitial: "DO";
-    _DocumentIdentifier: "$DELIVERY_ORDER";
-    _DocumentType: {
-        reference: "DEV/DocumentType/Records/TRANSACTION";
-    };
-}
+import { FirstLevelDocumentModel } from "../../model/FirstLevelDocument.model";
+
+const field: FirstLevelDocumentModel = {
+  TransactionNoInitial: "DO",
+  _DocumentIdentifier: "$DELIVERY_ORDER",
+  _DocumentType: {
+    reference: "DEV/DocumentType/Records/TRANSACTION",
+  },
+};
+
+export default field;
