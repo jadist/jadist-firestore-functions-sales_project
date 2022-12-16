5a53946a7fc3d038a461a15b31c955653b08c299
Eldest Pasirula <aknutman@gmail.com>
2022-12-17 00:40:47 +0700

    Updating CashRegistration.document.field

--- functions/src/firestore/DEV.collection/CashRegistration.document/field.ts
+++ functions/src/firestore/DEV.collection/CashRegistration.document/field.ts
@@ -1,7 +1,12 @@
-export interface field {
-    TransactionNoInitial: "CR";
-    _DocumentIdentifier: "$CASH_REGISTRATION";
-    _DocumentType: {
-        reference: "DEV/DocumentType/Records/TRANSACTION";
-    };
-}
+import { FirstLevelDocumentModel } from "../../model/FirstLevelDocument.model";
+
+const field: FirstLevelDocumentModel = {
+  TransactionNoInitial: "CR",
+  _DocumentIdentifier: "$CASH_REGISTRATION",
+  _DocumentType: {
+    reference: "DEV/DocumentType/Records/TRANSACTION",
+  },
+};
+
+export default field;
+
