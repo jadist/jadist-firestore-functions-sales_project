a310fc665529c3eac30f049fd17252565a921472
Eldest Pasirula <aknutman@gmail.com>
2022-12-16 23:08:01 +0700

    CashCollections.document

---
+++ functions/src/firestore/DEV.collection/CashCollections.document/Schema.collection/1.document/field.ts
@@ -0,0 +1,13 @@
+export interface field {
+    AsDeliveryManUserFullNameRef: string;
+    AsDeliveryManUserNotes: string;
+    AsDeliveryManUserReference: string;
+    CashAmount: number;
+    CashCollectionNo: string;
+    CashCollectionNotes: string;
+    CreatedDateTime: string;
+    ModifiedDateTime: Date;
+    PayerDescription: string;
+    PayerName: string;
+    _SchemaVersion: number;
+}
---
+++ functions/src/firestore/DEV.collection/CashCollections.document/field.ts
@@ -0,0 +1,7 @@
+export interface field {
+    TransactionNoInitial: "CC";
+    _DocumentIdentifier: "$CASH_COLLECTIONS";
+    _DocumentType: {
+        reference: "DEV/DocumentType/Records/TRANSACTION";
+    };
+}
