5c5082426d4f81bfd1284e7ae41e78e23654c3cd
Eldest Pasirula <aknutman@gmail.com>
2022-12-17 12:49:19 +0700

    PurchaseOrder.document

---
+++ functions/src/firestore/DEV.collection/PurchaseOrder.document/Schema.collection/1.document/field.ts
@@ -0,0 +1,10 @@
+interface field {
+  CreatedDateTime?: Date;
+  ModifiedDateTime?: Date;
+  PurchaseOrderNo: string;
+  PurchaseOrderNotes: string;
+  PurchaseOrderQuantity: number;
+  _SchemaVersion: number;
+}
+
+export default field;
