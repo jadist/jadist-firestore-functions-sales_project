407b408e85df1bd883ce4ec81fdb25632b6dacb4
Eldest Pasirula <aknutman@gmail.com>
2022-12-17 12:00:43 +0700

    Firestore Reference missing fix

--- functions/src/firestore/DEV.collection/Profiles.document/AccessSchema.collection/field.ts
+++ functions/src/firestore/DEV.collection/Profiles.document/AccessSchema.collection/field.ts
@@ -1,8 +1,6 @@
 import { FirestoreReference } from "../../../model/Firestore.model";
 interface field {
-  AccessTypeReference: {
-    reference: string;
-  };
+  AccessTypeReference: FirestoreReference;
   CreatedDateTime?: Date;
   DocumentIdentifierReference: FirestoreReference;
   ModifiedDateTime?: Date;
