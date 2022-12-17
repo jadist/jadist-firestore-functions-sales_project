046baf08e3837011efc7547b6dd180c93b8f34f1
Eldest Pasirula <aknutman@gmail.com>
2022-12-17 13:03:46 +0700

    ReceivedNotes.document

---
+++ functions/src/firestore/DEV.collection/ReceivedNotes.document/Schema.collection/1.document/field.ts
@@ -0,0 +1,11 @@
+interface field {
+  CreatedDateTime?: Date;
+  ModifiedDateTime?: Date;
+  ReceivedNo: string;
+  ReceivedNotes: string;
+  ReceiverName: string;
+  ReceiverPhotos: string;
+  _SchemaVersion: number;
+}
+
+export default field;
