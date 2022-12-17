ba05ae8609a62f29a95e43ddb8fc4fbd77eced36
Eldest Pasirula <aknutman@gmail.com>
2022-12-17 10:14:42 +0700

    DocumentTypeEnum

---
+++ functions/src/firestore/text/DocumentType.enum.ts
@@ -0,0 +1,8 @@
+export enum DocumentTypeEnum {
+  MASTER = "DEV/DocumentType/Records/MASTER",
+  NESTED_MASTER = "DEV/DocumentType/Records/NESTED_MASTER",
+  SYSTEM_MASTER = "DEV/DocumentType/Records/SYSTEM_MASTER",
+  SYSTEM_TRANSACTION = "DEV/DocumentType/Records/SYSTEM_TRANSACTION",
+  TRANSACTION = "DEV/DocumentType/Records/TRANSACTION",
+  TRANSACTION_CHAIN = "DEV/DocumentType/Records/TRANSACTION_CHAIN",
+}
