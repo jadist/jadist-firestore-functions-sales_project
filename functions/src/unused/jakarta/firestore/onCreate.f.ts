8bbf69021a6c08e0ec1c3bedad5d4f2892e196a5
Eldest Pasirula <aknutman@gmail.com>
2023-01-12 21:57:06 +0700

    testing to connect to firestore

---
+++ functions/src/unused/jakarta/firestore/onCreate.f.ts
@@ -0,0 +1,19 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+module.exports = functions
+  .region("asia-southeast2")
+  .https.onRequest((request, response) => {
+    admin
+      .firestore()
+      .collection("DELETE-ME-Collection")
+      .doc("DELETE-ME-Document")
+      .create({ name: "value" })
+      .then((result) => {
+        functions.logger.info(
+          "Try to write to firestore from functions - by EAP",
+          { structuredData: true }
+        );
+        response.send("Try to write to firestore from functions - by EAP");
+      });
+  });
