633b72e266dfda19d623a3757684a9a305e22ef5
Eldest Pasirula <aknutman@gmail.com>
2023-02-10 17:01:34 +0700

    cors and its uses sample

--- functions/src/jkt/user/role/searchView.f.ts
+++ functions/src/jkt/user/role/searchView.f.ts
@@ -3,49 +3,55 @@ import * as admin from "firebase-admin";
 import * as ur from "./userRole.model";
 import * as validation from "../../../helper/auth/firebase-validation";
 
+import * as corsModule from "cors";
+
+const cors = corsModule({ origin: true });
+
 module.exports = functions
   .region("asia-southeast2")
   .https.onRequest(async (request, response) => {
-    const jwt = String(request.headers.authorization?.split("Bearer ")[1]);
+    cors(request, response, async () => {
+      const jwt = String(request.headers.authorization?.split("Bearer ")[1]);
 
-    const result = await validation.validate(jwt);
+      const result = await validation.validate(jwt);
 
-    if (!result.IsTrue) {
-      response
-        .status(401)
-        .contentType("application/json")
-        .send(JSON.stringify(result));
-    }
+      if (!result.IsTrue) {
+        response
+          .status(401)
+          .contentType("application/json")
+          .send(JSON.stringify(result));
+      }
 
-    const filterString = String(request.query["search"]).toLowerCase();
+      const filterString = String(request.query["search"]).toLowerCase();
 
-    const docs = admin.firestore().collection(ur.UserRoleCollectionName);
-    const snapshot = await docs.get();
+      const docs = admin.firestore().collection(ur.UserRoleCollectionName);
+      const snapshot = await docs.get();
 
-    const arrData: Array<{
-      Id: string;
-      Data: ur.UserRoleDocumentModel;
-    }> = [];
+      const arrData: Array<{
+        Id: string;
+        Data: ur.UserRoleDocumentModel;
+      }> = [];
 
-    snapshot.forEach((doc) => {
-      const docData = <ur.UserRoleDocumentModel>doc.data();
+      snapshot.forEach((doc) => {
+        const docData = <ur.UserRoleDocumentModel>doc.data();
 
-      arrData.push({
-        Id: doc.id,
-        Data: docData,
+        arrData.push({
+          Id: doc.id,
+          Data: docData,
+        });
       });
+
+      const filteredData = arrData.filter((item) => {
+        const docKeys = Object.entries(item.Data);
+        const docValue = docKeys.map((docItem) => docItem[1]);
+
+        const filterExist = docValue.filter((docItem) =>
+          String(docItem).toLowerCase().includes(filterString)
+        );
+
+        return filterExist.length > 0;
+      });
+
+      response.send(filteredData);
     });
-
-    const filteredData = arrData.filter((item) => {
-      const docKeys = Object.entries(item.Data);
-      const docValue = docKeys.map((docItem) => docItem[1]);
-
-      const filterExist = docValue.filter((docItem) =>
-        String(docItem).toLowerCase().includes(filterString)
-      );
-
-      return filterExist.length > 0;
-    });
-
-    response.send(filteredData);
   });
--- functions/src/unused/jakarta/restricted/onView.f.ts
+++ functions/src/unused/jakarta/restricted/onView.f.ts
@@ -1,21 +1,26 @@
 import * as functions from "firebase-functions";
 import * as validation from "../../../helper/auth/firebase-validation";
+import * as corsModule from "cors";
+
+const cors = corsModule({ origin: true });
 
 module.exports = functions
   .region("asia-southeast2")
   .https.onRequest(async (request, response) => {
-    const jwt = String(request.headers.authorization?.split("Bearer ")[1]);
+    cors(request, response, async () => {
+      const jwt = String(request.headers.authorization?.split("Bearer ")[1]);
 
-    const result = await validation.validate(jwt);
+      const result = await validation.validate(jwt);
 
-    if (!result.IsTrue) {
+      if (!result.IsTrue) {
+        response
+          .status(401)
+          .contentType("application/json")
+          .send(JSON.stringify(result));
+      }
+
       response
-        .status(401)
         .contentType("application/json")
-        .send(JSON.stringify(result));
-    }
-
-    response
-      .contentType("application/json")
-      .send(JSON.stringify({ message: "OKE" }));
+        .send(JSON.stringify({ message: "OKE" }));
+    });
   });
